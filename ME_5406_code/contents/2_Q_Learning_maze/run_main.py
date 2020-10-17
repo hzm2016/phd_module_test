295f233845ed452358bbe87e5cac238ff971db34
hzm2016 <2539722953@qq.com>
2020-10-17 15:08:04 +0800

    “update”

--- ME_5406_code/contents/2_Q_Learning_maze/run_main.py
+++ ME_5406_code/contents/2_Q_Learning_maze/run_main.py
@@ -1,19 +1,20 @@
 """
 Reinforcement learning maze example.
 """
-from frozen_lake_env import Frozen_lake
-# from RL_brain import QLearningTable
-from all_algorithms import *
 # from algorithms import *
 import copy as cp
-import matplotlib.pyplot as plt
+
 import numpy as np
-import pandas as pd
+# from RL_brain import QLearningTable
+from all_algorithms import *
+from frozen_lake_env import Frozen_lake
 from result_analysis import *
+import matplotlib.pyplot as plt
+
 np.random.seed(0)
 
 
-def run(RL, algorithm_name=None, numEpisode=10, max_epsilon=0.98):
+def run(RL, env, algorithm_name=None, numEpisode=10, max_epsilon=0.98):
     reward_list = []
     num_steps_list = []
 
@@ -32,7 +33,7 @@ def run(RL, algorithm_name=None, numEpisode=10, max_epsilon=0.98):
 
         epi_reward = 0.0
         epi_num_steps = 0
-        while True:
+        while epi_num_steps < 100:
             # fresh env
             env.render()
 
@@ -47,7 +48,7 @@ def run(RL, algorithm_name=None, numEpisode=10, max_epsilon=0.98):
             epi_num_steps += 1
             if algorithm_name == "sarsa":
                 # RL choose action based on next observation
-                action_ = RL.choose_action(state_)
+                action_ = RL.choose_action(state_, epsilon=epislon_greedy)
                 # RL learn from this transition
                 RL.learn(state, action, reward, state_, action_, done)
             elif algorithm_name == "expected-sarsa":
@@ -83,75 +84,91 @@ if __name__ == "__main__":
     algorithm_list = ["FVMCWOES", "Q-learning", "sarsa", "expected-sarsa"]
     algorithm = algorithm_list[3]
     parameters_lr = [1.0, 0.1, 0.01]
-    parameters_epsilon = [0.99, 0.9, 0.85, 0.6]
+    parameters_discount_rate = [1.0, 0.99, 0.9, 0.7, 0.3]
+    parameters_epsilon = [0.99, 0.9, 0.85, 0.6, 0.3, 0.1]
 
     reward_list = []
     value_list = []
     num_steps_list = []
 
-    para_name = "_epsilon_"
-    parameter_list = parameters_epsilon
+    # para_name = "_epsilon_"
+    # parameter_list = parameters_epsilon
 
+    # para_name = "_lr_"
+    # parameter_list = parameters_lr
+    
+    para_name = "_discount_rate_"
+    parameter_list = parameters_discount_rate
+
+    run_plot = False
+    if run_plot == True:
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    for para in parameter_list:
-        # set up environment
-        env = Frozen_lake()
+        for para in parameter_list:
+            # set up environment
+            env = Frozen_lake()
 
-        # set algorithm
-        if algorithm == "sarsa":
-            RL = SarsaTable(actions=list(range(env.n_actions)),
-                            states=list(range(env.n_states)),
-                            learning_rate=0.1, reward_decay=0.9, e_greedy=para)
-        elif algorithm == "expected-sarsa":
-            RL = ExpectSarsaTable(actions=list(range(env.n_actions)),
-                            states=list(range(env.n_states)),
-                            learning_rate=0.1, reward_decay=0.9, e_greedy=para)
-        elif algorithm == "Q-learning":
-            RL = QLearningTable(actions=list(range(env.n_actions)),
+            print("algorithm ：:", algorithm)
+            # set algorithm
+            if algorithm == "sarsa":
+                RL = SarsaTable(actions=list(range(env.n_actions)),
                                 states=list(range(env.n_states)),
-                                learning_rate=0.1, reward_decay=0.9, e_greedy=para)
-        else:
-            s_a_value = monteCarloNoES(env, numEpisode=20, gamma=1.0, epsilon=0.1)
+                                learning_rate=parameters_lr[1],
+                                reward_decay=para,
+                                e_greedy=parameters_epsilon[0])
+            elif algorithm == "expected-sarsa":
+                RL = ExpectSarsaTable(actions=list(range(env.n_actions)),
+                                      states=list(range(env.n_states)),
+                                      learning_rate=parameters_lr[1],
+                                      reward_decay=para,
+                                      e_greedy=parameters_epsilon[0])
+            elif algorithm == "Q-learning":
+                RL = QLearningTable(actions=list(range(env.n_actions)),
+                                    states=list(range(env.n_states)),
+                                    learning_rate=parameters_lr[1],
+                                    reward_decay=para,
+                                    e_greedy=parameters_epsilon[0])
+            else:
+                s_a_value = monteCarloNoES(env, numEpisode=20, gamma=1.0, epsilon=0.1)
+    
+            reward, num_steps, value = run(RL, env, algorithm_name=algorithm,
+                                           numEpisode=100,
+                                           max_epsilon=parameter_list[0])
+            reward_list.append(cp.deepcopy(reward))
+            num_steps_list.append(cp.deepcopy(num_steps))
+            value_list.append(cp.deepcopy(value))
+    
+        np.save("./0-data/" + algorithm + para_name + "-lr-value-list.npy", np.array(value_list))
+        np.save("./0-data/" + algorithm + para_name + "-lr-reward-list.npy", np.array(reward_list))
+        np.save("./0-data/" + algorithm + para_name + "-lr-num-steps-list.npy", np.array(num_steps_list))
+    else:
+        # plot results
+        # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        value_list = np.load("./0-data/" + algorithm + para_name + "-lr-value-list.npy")
+        reward_list = np.load("./0-data/" + algorithm + para_name + "-lr-reward-list.npy")
+        num_steps_list = np.load("./0-data/" + algorithm + para_name + "-lr-num-steps-list.npy")
 
-        reward, num_steps, value = run(RL, algorithm_name=algorithm,
-                                       numEpisode=100, max_epsilon=0.98)
-        reward_list.append(cp.deepcopy(reward))
-        num_steps_list.append(cp.deepcopy(num_steps))
-        value_list.append(cp.deepcopy(value))
+        # print(num_steps_list)
+        #
+        fig = plt.figure(figsize=(8, 4))
+        plt.title(algorithm)
+        para_name = 'Lr_'
+        for index, reward in enumerate(reward_list):
+            plt.plot(np.array(reward), label=para_name + str(index))
 
-    np.save("./0-data/" + algorithm + para_name + "-lr-value-list.npy", np.array(value_list))
-    np.save("./0-data/" + algorithm + para_name + "-lr-reward-list.npy", np.array(reward_list))
-    np.save("./0-data/" + algorithm + para_name + "-lr-num-steps-list.npy", np.array(num_steps_list))
+        plt.xlabel('Episodes')
+        plt.ylabel('Episode Reward')
+        plt.savefig("1-figure/" + algorithm + para_name + '_reward.png')
+        plt.legend(loc=2, bbox_to_anchor=(1.05, 1.0))
+        plt.show()
 
-    # plot results
-    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    # value_list = np.load("./0-data/" + algorithm + para_name + "-lr-value-list.npy")
-    # reward_list = np.load("./0-data/" + algorithm + para_name + "-lr-reward-list.npy")
-    # num_steps_list = np.load("./0-data/" + algorithm + para_name + "-lr-num-steps-list.npy")
-    #
-    # print(num_steps_list)
-    #
-    # fig = plt.figure(figsize=(8, 4))
-    # plt.title(algorithm)
-    # para_name = 'Lr_'
-    # for index, reward in enumerate(reward_list):
-    #     plt.plot(np.array(reward), label=para_name + str(index))
-    #
-    # plt.xlabel('Episodes')
-    # plt.ylabel('Episode Reward')
-    # plt.savefig("1-figure/" + algorithm + para_name + '_reward.png')
-    # plt.legend(loc=2, bbox_to_anchor=(1.05, 1.0))
-    # plt.show()
-    #
-    # fig = plt.figure(figsize=(8, 4))
-    # plt.title(algorithm)
-    # para_name = 'Lr_'
-    # for index, reward in enumerate(num_steps_list):
-    #     plt.plot(np.array(reward), label=para_name + str(index))
-    #
-    # plt.xlabel('Episodes')
-    # plt.ylabel('Episode Steps')
-    # plt.savefig("1-figure/" + algorithm + para_name + '_steps.png')
-    # plt.legend(loc=2, bbox_to_anchor=(1.05, 1.0))
-    # plt.show()
+        fig = plt.figure(figsize=(8, 4))
+        plt.title(algorithm)
+        para_name = 'Lr_'
+        for index, reward in enumerate(num_steps_list):
+            plt.plot(np.array(reward), label=para_name + str(index))
 
+        plt.xlabel('Episodes')
+        plt.ylabel('Episode Steps')
+        plt.savefig("1-figure/" + algorithm + para_name + '_steps.png')
+        plt.legend(loc=2, bbox_to_anchor=(1.05, 1.0))
+        plt.show()
